53ee8d26b7c229a610c54dce5f3b450b97e95284
Aman <thisisamandang@gmail.com>
2023-05-17 20:30:29 +0530

    fixes

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
-
 function NavBar() {
   return (
     <div className="w-full flex justify-center">
-      <img src="/logo.png" alt="logo" className="w-24 h-24 mt-2 " />
+      <img
+        src="https://raw.githubusercontent.com/thisisamandang/github-profile-markdown-generator/main/public/logo.png"
+        alt="logo"
+        className="w-24 h-24 mt-2 "
+      />
     </div>
   );
 }
