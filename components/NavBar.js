de2177c765a1c4ecef5cfe7c461bf79def0cde90
Nkiriobasi <obasinkiri@gmail.com>
2023-09-18 14:25:44 +0100

    implemented light and dark theme

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,19 @@
 import React from "react";
+import ThemeButton from "./UI/themeButton";
 function NavBar() {
   return (
-    <div className="w-full flex justify-center">
-      <img
-        src="https://raw.githubusercontent.com/thisisamandang/github-profile-markdown-generator/main/public/logo.png"
-        alt="logo"
-        className="w-24 h-24 mt-2 "
-      />
+    <div className="w-full flex justify-center items-center">
+      <div className="w-[50%] flex justify-end items-center">
+        <img
+          src="https://raw.githubusercontent.com/thisisamandang/github-profile-markdown-generator/main/public/logo.png"
+          alt="logo"
+          className="w-24 h-24 mt-2"
+        />
+      </div>
+      
+      <div className="w-[50%] flex justify-end items-center mr-8">
+        <ThemeButton />
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 }
