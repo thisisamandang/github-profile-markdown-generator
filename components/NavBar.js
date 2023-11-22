f061bf0a3b7f0fbded95e2cc1c334f030ba73aa8
thisisamandang <thisisamandang@gmail.com>
2023-11-23 02:59:29 +0530

    reverted dark mode

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,17 +3,17 @@ import ThemeButton from "./UI/themeButton";
 function NavBar() {
   return (
     <div className="w-full flex justify-center items-center">
-      <div className="w-[50%] flex justify-end items-center">
+      <div className="w-[50%] flex justify-center items-center">
         <img
           src="https://raw.githubusercontent.com/thisisamandang/github-profile-markdown-generator/main/public/logo.png"
           alt="logo"
           className="w-24 h-24 mt-2"
         />
       </div>
-      
-      <div className="w-[50%] flex justify-end items-center mr-8">
+
+      {/* <div className="w-[50%] flex justify-end items-center mr-8">
         <ThemeButton />
-      </div>
+      </div> */}
     </div>
   );
 }
